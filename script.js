551931efe9caf140e86381fe06b855888d6a0df4
Lephuong96 <bigankroi@gmail.com>
2018-10-01 21:25:19 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function processConfirm(answer) {
+    var result = "";
+    if (answer){
+        result = "Excellent.We'll play a nuce game of chess.";
+    }else {
+        result = " Maybe later then.";
+    }
+    return result;
+}
